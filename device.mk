9675a5ad8c0f9c91c7ed154f4a35ca01edcfebed
aragon12 <sm95007@gmail.com>
2018-07-21 03:41:25 +0000

    rosy: switch to prebuilt fingerprintd

--- device.mk
+++ device.mk
@@ -257,7 +257,3 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
-
-# Fingerprint
-PRODUCT_PACKAGES += \
-    fingerprintd
