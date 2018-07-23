db872e669ec31427f747c6ad6ee70e1b2b507af8
HeeRyun, Jin <jins4218@gmail.com>
2018-07-23 10:50:35 +0000

    rosy: build custom lights HAL

--- liblight/a.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-
-"""
-File    : a.py
-Project :
-Author  : ze00
-Email   : zerozakiGeek@gmail.com
-"""
-print(list(reversed([int(i) for i in open("lightsMap").read().split()])))
